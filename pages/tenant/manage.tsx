edb8faa3b10d4ccfd35eec27f3cbbe51fdc61fd7
Emeka Mamah <mamahemeka84@gmail.com>
2022-08-11 15:45:35 +0100

    update

--- pages/tenant/manage.tsx
+++ pages/tenant/manage.tsx
@@ -22,7 +22,7 @@ const ManageTenants: NextPage = () => {
                             </div>) :
                         data?.getUserTenants?.map((tenant: any, index: number) => (
                             <div key={index} className={'w-full flex flex-col md:flex-row md:justify-between mb-4 pb-2 border-b border-b-2 border-b-gray-200 capitalize'}>
-                                <div className={'w-full md:w-1/2 flex items-start'}>
+                                <div className={'w-full md:w-1/2 flex items-start mb-3'}>
                                     <Image src={tenant?.medially[0]?.fileUrl} width={50} height={50}/>
                                     <div className={'ml-3 text-left text-sm'}>
                                         <p className={'text-xl font-semibold mb-2'}>{tenant?.name}</p>
@@ -35,7 +35,7 @@ const ManageTenants: NextPage = () => {
                                     <ul className={'text-sm'}>
                                         {/*<li className={'mb-2 pb-1 border-b border-b-transparent hover:border-black'}>*/}
                                         {/*    <Link href={''}>*/}
-                                        {/*        <a className={'flex justify-between'}>*/}
+                                        {/*        <a className={'w-full  flex justify-between'}>*/}
                                         {/*            <span className={"mr-6"}>View Tenant report</span>*/}
                                         {/*            <span> {'>'} </span>*/}
                                         {/*        </a>*/}
@@ -43,7 +43,7 @@ const ManageTenants: NextPage = () => {
                                         {/*</li>*/}
                                         <li className={'mb-2 pb-1 border-b border-b-transparent hover:border-black'}>
                                             <Link href={`/tenant/notify/${tenant.id}`}>
-                                                <a className={'flex justify-between'}>
+                                                <a className={'w-full  flex justify-between'}>
                                                     <span className={"mr-6"}>Send Notification</span>
                                                     <span> {'>'} </span>
                                                 </a>
@@ -51,7 +51,7 @@ const ManageTenants: NextPage = () => {
                                         </li>
                                         <li className={'mb-2 pb-1 border-b border-b-transparent hover:border-black'}>
                                             <Link href={`/tenant/bill/${tenant.id}/`}>
-                                                <a className={'flex justify-between'}>
+                                                <a className={'w-full  flex justify-between'}>
                                                     <span className={"mr-6"}>Generate Bill</span>
                                                     <span> {'>'} </span>
                                                 </a>
@@ -59,7 +59,7 @@ const ManageTenants: NextPage = () => {
                                         </li>
                                         <li className={'mb-2 pb-1 border-b border-b-transparent hover:border-black'}>
                                             <Link href={`/tenant/receipt/${tenant?.id}`}>
-                                                <a className={'flex justify-between'}>
+                                                <a className={'w-full  flex justify-between'}>
                                                     <span className={"mr-6"}>Generate Receipt</span>
                                                     <span> {'>'} </span>
                                                 </a>
